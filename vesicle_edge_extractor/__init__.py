3b54e09b6626452a4bc13dc5c86ec3713cfbf399
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-04-06 14:25:58 -0400

    fixed typo

--- vesicle_edge_extractor/__init__.py
+++ vesicle_edge_extractor/__init__.py
@@ -9,12 +9,12 @@ __author__ = 'Brannigan Lab'
 __credits__ = 'Rutgers University - Camden'
 __all__=['VesicleVideo',
         'edge_extractor',
-	'vesicel_video_utils',
+	'vesicle_video_utils',
         'single_spectrum',
         'spectrum_utils']
 
 from .vesicle_video import *
 from .edge_extractor import *
 from .vesicle_video_utils import *
-from single_spectrum import *
-from spectrum_utils import *
+from .single_spectrum import *
+from .spectrum_utils import *
