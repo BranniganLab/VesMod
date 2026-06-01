378a5fe18ed0205df65730ef8b3b448faf20d2dd
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-06-01 13:56:10 -0400

    fixed typo again?

--- vesmod/VesEdge/__init__.py
+++ vesmod/VesEdge/__init__.py
@@ -2,4 +2,4 @@
 from .vesicle_video import VesicleVideo
 from .edge_extractor import extract_edge_from_frame
 
-__all__ = [VesicleoVide, extract_edge_from_frame]
+__all__ = [VesicleoVideo, extract_edge_from_frame]
