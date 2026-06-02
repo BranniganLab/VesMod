0ffdade430c231b3e8698b6ca0a970e053633f7f
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-06-02 10:39:42 -0400

    updated example usage

--- VesEdge_example_usage.py
+++ VesEdge_example_usage.py
@@ -8,8 +8,8 @@ import glob
 import nd2
 from vesmod.VesEdge import VesicleVideo, extract_edge_from_frame
 
-fpath = "YOUR PATH HERE"  # path to the directory containing your .nd2 file(s)
-pixel_to_micron_ratio = "YOUR RATIO HERE"  # How many pixels to 1 micron in your micrscope image?
+fpath = "YOUR/PATH/HERE/"  # path to the directory containing your .nd2 file(s)
+micron_to_pixel_ratio = 1 / 13.44  # How many microns:pixels in your micrscope image?
 
 for file in glob.glob(fpath + '*.nd2', recursive=True):
     path = Path(file).resolve()
@@ -18,7 +18,7 @@ for file in glob.glob(fpath + '*.nd2', recursive=True):
         # skip this file because edge extraction already performed
         continue
     intensities = nd2.imread(path)
-    video = VesicleVideo(intensities, pixel_to_micron_ratio)
+    video = VesicleVideo(intensities, micron_to_pixel_ratio)
     video.extract_edges(extract_edge_from_frame, curvature_threshold=10)
     video.make_vesicle_gif(path, show_trace=True)
     video.save_edge_to_npy(path)
