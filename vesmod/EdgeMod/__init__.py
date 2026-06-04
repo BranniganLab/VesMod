0502cf8d97cd8f58059a42897f04d17eb8df9b89
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-06-04 15:13:16 -0400

    fixed bug

--- vesmod/EdgeMod/__init__.py
+++ vesmod/EdgeMod/__init__.py
@@ -1,5 +1,5 @@
 """Import the necessary files."""
-from .single_spectrum import Spectrum
+from .spectrum import Spectrum
 from .average_of_spectra import AverageOfSpectra
 
 __all__ = ["Spectrum", "AverageOfSpectra"]
