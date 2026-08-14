97bdf4b0abdb0d947e6228d7cebb4adc8f3768e7
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-08-14 16:28:15 -0400

    refactor vesicle_video

--- vesmod/VesEdge/vesicle_video.py
+++ vesmod/VesEdge/vesicle_video.py
@@ -7,15 +7,68 @@ Created on Wed Nov  5 10:42:28 2025.
 """
 from dataclasses import dataclass, field
 from pathlib import Path
+from collections.abc import Callable
 import traceback
 import numpy as np
 from numpy.typing import NDArray
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from .vesicle_video_utils import convert_to_cartesian, measure_wrapped_finite_second_difference, downsample_to_new_indices
-from .edge_filtering import EdgeQC
+from .edge_filtering import EdgeQC, EdgeQCConfig
 
 type EdgeResult = EdgeDetection | EdgeDetectionFailure
+
+
+@dataclass(frozen=True)
+class ImageContour:
+    """
+    The polar and Cartesian coordinates of a contour in the frame of reference \
+    of the image they are extracted from.
+
+    Attributes
+    ----------
+    origin : tuple(float, float)
+        Cartesian coordinates (x, y) of the origin point in the original image's
+        frame of reference.
+    r : NDArray[np.float64]
+        The distances from `center` to the edge of the vesicle. Evenly spaced
+        in theta, ranging from 0 to 2pi (not inclusive).
+    theta : NDArray[np.float64]
+        Evenly spaced angular values ranging from 0 to 2pi (not inclusive).
+        Step size is determined by how many values are in `r`.
+    x, y : NDArray[np.float64]
+        The Cartesian coordinates of the Contour. First and last bins overlap
+        for plotting purposes.
+    """
+
+    origin: tuple[float, float]
+    r: NDArray[np.float64]
+
+    @property
+    def theta(self) -> NDArray[np.float64]:
+        """Evenly spaced angular values ranging from 0 to 2pi (not inclusive)."""
+        return np.linspace(0, 2 * np.pi, self.r.shape[0], endpoint=False)
+
+    @property
+    def x(self) -> NDArray[np.float64]:
+        """
+        The x-coordinates of the contour.
+
+        First and last bins overlap for plotting purposes.
+        """
+        x_vals = self.r * np.cos(self.theta) + self.origin[0]
+        return np.append(x_vals, x_vals[0])
+
+    @property
+    def y(self) -> NDArray[np.float64]:
+        """
+        The y-coordinates of the contour.
+
+        First and last bins overlap for plotting purposes.
+        """
+        y_vals = self.r * np.sin(self.theta) + self.origin[1]
+        return np.append(y_vals, y_vals[0])
+
 
 @dataclass
 class EdgeDetection:
@@ -24,21 +77,13 @@ class EdgeDetection:
 
     Attributes
     ----------
-    center : tuple(float, float)
-        Cartesian coordinates (x, y) of the approximate vesicle center for each
-        frame. Needed for wrapping images to/from polar coordinates.
-    sampled_radii : NDArray[np.float64]
-        The distances from `center` to the edge of the vesicle.
-        Evenly spaced in theta, ranging from 0 to 2pi (not inclusive).
-    sampled_theta : NDArray[np.float64]
-        Evenly spaced angular values ranging from 0 to 2pi (not inclusive).
-        Step size is determined by how many values are in `sampled_radii`.
-    contour_x, contour_y : NDArray[np.float64]
-        The Cartesian coordinates of the full detected edge prior to down-
-        sampling. First and last bins overlap for plotting purposes.
-    sampled_x, sampled_y : NDArray[np.float64]
-        The Cartesian coordinates of the downsampled detected edge. First and
-        last bins overlap for plotting purposes.
+    contour : VesicleContour
+        The full detected contour.
+    analysis_contour : VesicleContour
+        The contour points after downsampling (if performed).
+    radii_microns : NDArray[np.float64]
+        The radial profile of the vesicle, derived from the `analysis_contour`
+        and the `pixels_per_micron`.
     qc : EdgeQC
         Quality control information from the `edge_filtering` module.
     median_radius : float
@@ -47,43 +92,15 @@ class EdgeDetection:
         Whether or not this `EdgeDetection` passed QC in `edge_filtering`.
     """
 
-    center: tuple[float, float]
-    sampled_radii: NDArray[np.float64]
-    contour_x: NDArray[np.float64]
-    contour_y: NDArray[np.float64]
+    full_contour: ImageContour
+    analysis_contour: ImageContour
+    radii_microns: NDArray[np.float64]
     qc: EdgeQC = field(default_factory=EdgeQC)
-
-    @property
-    def sampled_theta(self) -> NDArray[np.float64]:
-        """Evenly spaced angular values ranging from 0 to 2pi (not inclusive)."""
-        return np.linspace(0, 2 * np.pi, self.sampled_radii.shape[0], endpoint=False)
-
-    @property
-    def sampled_x(self) -> NDArray[np.float64]:
-        """
-        The x-coordinates of the contour represented as r(theta) on the \
-        standardized angular grid.
-
-        First and last bins overlap for plotting purposes.
-        """
-        x_vals = self.sampled_radii * np.cos(self.sampled_theta) + self.center[0]
-        return np.append(x_vals, x_vals[0])
-
-    @property
-    def sampled_y(self) -> NDArray[np.float64]:
-        """
-        The y-coordinates of the contour represented as r(theta) on the \
-        standardized angular grid.
-
-        First and last bins overlap for plotting purposes.
-        """
-        y_vals = self.sampled_radii * np.sin(self.sampled_theta) + self.center[1]
-        return np.append(y_vals, y_vals[0])
 
     @property
     def median_radius(self) -> float:
         """The median radius value from this `EdgeDetection`."""
-        return float(np.median(self.sampled_radii))
+        return float(np.median(self.radii_microns))
 
     @property
     def accepted(self) -> bool:
@@ -93,7 +110,46 @@ class EdgeDetection:
 
 @dataclass(frozen=True)
 class EdgeDetectionFailure:
+    """Holds an error message upon Edge Detection failing due to an error."""
+
     error: str
+
+
+@dataclass(frozen=True)
+class EdgeExtractionConfig:
+    """
+    Configuration parameters for the edge extractor.
+
+    Attributes
+    ----------
+    pixels_per_micron : float
+        How many pixels in the image represent one micron in real space.
+    n_angular_samples : int | None
+        How many angular samples to downsample to. If None, do not downsample.
+    """
+
+    pixels_per_micron: float = 1
+    n_angular_samples: int | None = 120
+
+    def __post_init__(self) -> None:
+        """
+        Do argument validation.
+
+        Raises
+        ------
+        TypeError
+            If `n_angular_samples` is not `int` or `None`.
+        ValueError
+            If `pixels_per_micron` is not positive.
+            If `n_angular_samples` is not positive.
+        """
+        if self.pixels_per_micron <= 0:
+            raise ValueError("pixels_per_micron must be positive.")
+        if isinstance(self.n_angular_samples, int):
+            if self.n_angular_samples <= 0:
+                raise ValueError("n_angular_samples must be positive")
+        elif not isinstance(self.n_angular_samples, type(None)):
+            raise TypeError("n_angular_samples must be an int or None.")
 
 
 @dataclass
@@ -107,16 +163,20 @@ class VesicleVideo:
     ----------
         frames : numpy ndarray
             The 3D array of raw images. 0th dimension is frame number.
-        micron_to_pixel_ratio : float
-            The number of microns to pixels in your microscope image.
+        extraction_config : EdgeExtractorConfig
+            The configuration parameters for the edge extractor.
+        qc_config : EdgeQCConfig
+            The configuration parameters for the quality control checks.
+        detections : list[EdgeResult]
+            The edge detections for each frame.
     """
 
     frames: np.ndarray
-    micron_to_pixel_ratio: float
-    n_angular_samples: int | None = 120
+    extraction_config = EdgeExtractionConfig
+    qc_config: EdgeQCConfig
     detections: list[EdgeResult] = []
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         """
         Do argument validation.
 
@@ -127,100 +187,70 @@ class VesicleVideo:
         IndexError
             If frames is not a 3D ndarray.
             If n_angular_samples is greater than the number of samples.
-        ValueError
-            If pixel_to_micron_ratio is not positive.
-            If n_angular_samples is not positive.
-            if n_angular_samples is not an int or None.
-
-        Returns
-        -------
-        None.
-
         """
         if not isinstance(self.frames, np.ndarray):
             raise TypeError("frames must be a numpy ndarray.")
         if len(self.frames.shape) != 3:
             raise IndexError("frames must be a 3D array.")
-        if self.micron_to_pixel_ratio <= 0:
-            raise ValueError("pixel_to_micron_ratio must be positive.")
-        if isinstance(self.n_angular_samples, int):
-            if self.n_angular_samples <= 0:
-                raise ValueError("n_angular_samples must be positive")
-            if self.n_angular_samples > self.frames.shape[1]:
-                raise IndexError(f"Cannot downsample r_vals with len {self.frames.shape[1]} to {self.n_angular_samples}.")
-        elif not isinstance(self.n_angular_samples, type(None)):
-            raise ValueError("n_angular_samples must be an int or None.")
+        if self.extraction_config.n_angular_samples > self.frames.shape[1]:
+            raise IndexError(f"Cannot downsample r_vals with len {self.frames.shape[1]} to {self.extraction_config.n_angular_samples}.")
 
-    def extract_edges(self, extractor_func, curvature_threshold=5):
+    def extract_edges(self, extractor_func: Callable[NDArray[np.float64]]):
         """
-        Extract edges from every frame.
+        Extract edges from every frame and save as `EdgeDetection`.
 
-        Frames that fail edge extraction are marked with `EdgeDetectionFailure`.
+        Frames that produce errors are saved as `EdgeDetectionFailure`.
+
+        Parameters
+        ----------
+        extractor_func : Callable
+            The extractor function you wish to use to extract edges. Must take
+            a 2D numpy array as an input (one frame). Must output a 1D NDArray
+            of radii and a tuple containing the vesicle center in (y,x) format.
         """
-        for frame_num, frame in enumerate(self.frames):
+        for frame in self.frames:
             try:
-                r_vals, vesicle_center = extractor_func(self.frames[frame_num, :, :])
-                if r_vals.ndim != 1:
-                    raise ValueError("Extractor must return a 1D array of r-values.")
-                detected_edge = self._add_edge_to_video_frame(
-                    frame_num,
-                    r_vals,
-                    vesicle_center,
-                    curvature_threshold,
-                )
+                r_vals, vesicle_center = extractor_func(frame)
+                self._validate_extractor_results(r_vals)
+                detected_edge = self._compile_edge_detection_results(r_vals, vesicle_center)
             except Exception as error:
-                print(f"Error on frame {frame_num}")
                 traceback.print_exc()
                 self.detections.append(EdgeDetectionFailure(str(error)))
                 continue
 
-            self._run_frame_qc(frame, detected_edge, curvature_threshold)
+            self._run_frame_qc(frame, detected_edge)
             self.detections.append(detected_edge)
         self._run_trajectory_qc()
 
-    def _add_edge_to_video_frame(self, frame_num, r_vals, vesicle_center, curvature_threshold):
+    def _compile_edge_detection_results(self, r_vals, vesicle_center):
         """
         Save detected edge information for a given frame.
 
         Parameters
         ----------
-        frame_num : int
-            The frame number.
         r_vals : list or numpy ndarray
             The list or 1D array of radial distances from the vesicle_center,
             spaced evenly from 0 to 2pi.
         vesicle_center : tuple
             The origin (in y, x) of the polar coordinate system.
-        curvature_threshold : float
-            The level of curvature allowed between two contiguous r_vals before
-            edge extraction would be deemed unreliable.
 
         Returns
         -------
         None.
 
         """
-        x_vals, y_vals = convert_to_cartesian((vesicle_center[1], vesicle_center[0],), r_vals)
-        if self.n_angular_samples is not None:
-            r_vals = self._downsample_r_vals(r_vals, self.n_angular_samples)
-        rescaled_r = r_vals * self.micron_to_pixel_ratio
-
-        edge = EdgeDetection(
-            (vesicle_center[1], vesicle_center[0]),
-            rescaled_r,
-            x_vals,
-            y_vals,
-        )
-        
-        self.detections[frame_num] = edge
-
-        finite_second_difference = measure_wrapped_finite_second_difference(r_vals)
-        if (np.fabs(finite_second_difference) >= curvature_threshold).any():
-            self.status[frame_num] = 3
-        elif np.isnan(finite_second_difference).any():
-            self.status[frame_num] = 2
+        center = (vesicle_center[1], vesicle_center[0])
+        full_contour = ImageContour(center, r_vals)
+        if self.extraction_config.n_angular_samples is not None:
+            downsampled_r_vals = self._downsample_r_vals(r_vals, self.extraction_config.n_angular_samples)
+            analysis_contour = ImageContour(center, downsampled_r_vals)
+            rescaled_r = downsampled_r_vals * self.extraction_config.pixels_per_micron
         else:
-            self.status[frame_num] = 1
+            analysis_contour = full_contour
+            rescaled_r = r_vals * self.extraction_config.pixels_per_micron
+
+        edge = EdgeDetection(full_contour, analysis_contour, rescaled_r)
+        return edge
 
     def _downsample_r_vals(self, r_vals: np.ndarray, n_samples: int = 120) -> np.ndarray:
         """
@@ -261,6 +291,13 @@ class VesicleVideo:
         new_evenly_spaced_indices = zero_to_ntheta * (r_vals.shape[0] / n_samples)
         downsampled_r_vals = downsample_to_new_indices(r_vals, new_evenly_spaced_indices)
         return downsampled_r_vals
+
+    def _validate_extractor_results(self, r_vals):
+        """Check to make sure extractor returns a 1D ndarray."""
+        if not isinstance(r_vals, np.ndarray):
+            raise TypeError(f"Extractor must return an NDArray, not {type(r_vals)}.")
+        if r_vals.ndim != 1:
+            raise ValueError("Extractor must return a 1D array of r-values.")
 
     def make_vesicle_gif(self, path, show_trace=True):
         """
