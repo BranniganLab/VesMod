de3859e90b9cc0d9b1d634be2d789ebf1f2405ff
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-08-14 17:59:16 -0400

    added config to public API

--- vesmod/VesEdge/__init__.py
+++ vesmod/VesEdge/__init__.py
@@ -1,5 +1,11 @@
 """Import the necessary files."""
-from .vesicle_video import VesicleVideo
+from .vesicle_video import VesicleVideo, EdgeExtractionConfig
 from .edge_extractor import extract_edge_from_frame
+from .edge_filtering import EdgeQCConfig
 
-__all__ = ["VesicleVideo", "extract_edge_from_frame"]
+__all__ = [
+    "VesicleVideo",
+    "EdgeExtractionConfig",
+    "extract_edge_from_frame",
+    "EdgeQCConfig",
+]
