1b8d46c111313b379382432b3252fbcbb9f26197
Jesse Sandberg <sandberg.jesse@gmail.com>
2026-08-17 20:48:01 -0400

    updated init

--- vesmod/VesEdge/__init__.py
+++ vesmod/VesEdge/__init__.py
@@ -2,10 +2,15 @@
 from .vesicle_video import VesicleVideo, EdgeExtractionConfig
 from .edge_extractor import extract_edge_from_frame
 from .edge_filtering import EdgeQCConfig
+from .models import EdgeDetection, EdgeDetectionFailure, EdgeResult, QCFlag
 
 __all__ = [
+    "EdgeDetection",
+    "EdgeDetectionFailure",
     "VesicleVideo",
     "EdgeExtractionConfig",
+    "EdgeResult",
+    "QCFlag",
     "extract_edge_from_frame",
     "EdgeQCConfig",
 ]
