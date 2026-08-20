09d54efe7539744cf29ae046e88d38858e52d1e1
Jesse Sandberg <sandberg.jesse@gmail.com>
2026-08-20 11:14:36 -0400

    fixed stale usage

--- examples/EdgeMod_example_usage.py
+++ examples/EdgeMod_example_usage.py
@@ -8,7 +8,6 @@ import glob
 from vesmod.EdgeMod import Spectrum
 
 fpath = "YOUR/PATH/HERE/"  # path to the directory containing your .npy file(s)
-n_theta = 120              # Number of angular bins to downsample to
 lower_fitting_bound = 3    # Lowest mode to fit to
 upper_fitting_bound = 8    # Ignore this mode and above when fitting
 lmax = 500                 # Maximum summation index
@@ -17,7 +16,7 @@ free_sigma = True          # Fit surface tension (sigma) in addition to kc
 for file in glob.glob(fpath + '*.npy', recursive=True):
     path = Path(file).resolve()
     print(f"working on file {path.stem}")
-    spectrum = Spectrum(file, Ntheta=n_theta)
+    spectrum = Spectrum(file)
     kc, sigma = spectrum.extract_kc_from_fit(
         lower_bound=lower_fitting_bound,
         upper_bound=upper_fitting_bound,
