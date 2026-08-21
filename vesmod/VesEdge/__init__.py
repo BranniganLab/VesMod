24bd7e469819bd05210631f5dae325d4cb59995f
JesseSandberg <sandberg.jesse@gmail.com>
2026-08-21 17:52:50 -0400

    Export VesicleEdges from VesEdge

--- vesmod/VesEdge/__init__.py
+++ vesmod/VesEdge/__init__.py
@@ -1,8 +1,10 @@
-"""Import the necessary files."""
-from .vesicle_video import VesicleVideo, EdgeExtractionConfig
+"""Public VesEdge API."""
+
 from .edge_extractor import extract_edge_from_frame
 from .edge_filtering import EdgeQCConfig
 from .models import EdgeDetection, EdgeDetectionFailure, EdgeResult, QCFlag
+from .vesicle_edges import VesicleEdges
+from .vesicle_video import EdgeExtractionConfig, VesicleVideo
 
 __all__ = [
     "EdgeDetection",
@@ -11,6 +13,7 @@ __all__ = [
     "EdgeQCConfig",
     "EdgeResult",
     "QCFlag",
+    "VesicleEdges",
     "VesicleVideo",
     "extract_edge_from_frame",
 ]
