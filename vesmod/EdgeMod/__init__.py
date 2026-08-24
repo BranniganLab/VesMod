d210a5ac92800794217965ed3b0590813b5808ff
JesseSandberg <sandberg.jesse@gmail.com>
2026-08-24 11:04:34 -0400

    Export spectrum fit-range selectors

--- vesmod/EdgeMod/__init__.py
+++ vesmod/EdgeMod/__init__.py
@@ -1,5 +1,18 @@
 """Import the necessary files."""
+from .fit_range_selection import (
+    FitRangeSelection,
+    FitRangeSelector,
+    FixedFitRangeSelector,
+    QMinusThreeFitRangeSelector,
+)
 from .spectrum import Spectrum
 from .spectrum_ensemble import SpectrumEnsemble
 
-__all__ = ["Spectrum", "SpectrumEnsemble"]
+__all__ = [
+    "FitRangeSelection",
+    "FitRangeSelector",
+    "FixedFitRangeSelector",
+    "QMinusThreeFitRangeSelector",
+    "Spectrum",
+    "SpectrumEnsemble",
+]
