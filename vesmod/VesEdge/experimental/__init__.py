710ed965e085fae176bdb62d0d1109bdb673e136
JesseSandberg <sandberg.jesse@gmail.com>
2026-09-08 14:49:51 -0400

    Move QC schemas and results into check modules

--- vesmod/VesEdge/experimental/__init__.py
+++ vesmod/VesEdge/experimental/__init__.py
@@ -1,23 +1,30 @@
 """Experimental VesEdge APIs subject to change during method evaluation."""
 
-from .internal_structures import (
-    InternalStructureConfig,
-    InternalStructureFrameResult,
-    InternalStructureRegion,
-    InternalStructureVideoSummary,
-    detect_internal_structures,
-    summarize_internal_structures,
-)
-from ..config import InternalVesicleQCConfig
-from .internal_vesicle_qc import check_internal_vesicle_selection
+from .internal_vesicle_qc import InternalVesicleQCConfig, check_internal_vesicle_selection
 
 __all__ = [
     "InternalStructureConfig",
     "InternalStructureFrameResult",
     "InternalStructureRegion",
     "InternalStructureVideoSummary",
+    "InternalVesicleQCConfig",
+    "check_internal_vesicle_selection",
     "detect_internal_structures",
     "summarize_internal_structures",
-    "check_internal_vesicle_selection",
-    "InternalVesicleQCConfig",
 ]
+
+
+def __getattr__(name: str):
+    """Load optional internal-structure dependencies only when requested."""
+    if name not in {
+        "InternalStructureConfig",
+        "InternalStructureFrameResult",
+        "InternalStructureRegion",
+        "InternalStructureVideoSummary",
+        "detect_internal_structures",
+        "summarize_internal_structures",
+    }:
+        raise AttributeError(name)
+    from . import internal_structures
+
+    return getattr(internal_structures, name)
