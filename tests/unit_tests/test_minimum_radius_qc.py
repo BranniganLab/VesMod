4b6f484ad707de9f97950e3dc0ecf13d4bd07046
JesseSandberg <sandberg.jesse@gmail.com>
2026-09-08 11:17:14 -0400

    Test minimum radius orchestration and invalid radii

--- tests/unit_tests/test_minimum_radius_qc.py
+++ tests/unit_tests/test_minimum_radius_qc.py
@@ -1,6 +1,15 @@
 import numpy as np
+import pytest
 
-from vesmod.VesEdge import EdgeDetection, ImageContour
+from vesmod.VesEdge import (
+    CurvatureQCConfig,
+    EdgeDetection,
+    EdgeExtractionConfig,
+    EdgeQCConfig,
+    ImageContour,
+    MinimumRadiusQCConfig,
+    VesicleEdges,
+)
 from vesmod.VesEdge.edge_filtering import QCFlag
 from vesmod.VesEdge.minimum_radius_qc import check_minimum_radius
 
@@ -25,4 +34,29 @@ def test_minimum_radius_qc_accepts_threshold_boundary():
 
 def test_minimum_radius_qc_is_disabled_by_orchestration_config():
     detection = edge([1.0] * 8)
+    edges = VesicleEdges(EdgeExtractionConfig(), [detection])
+    config = EdgeQCConfig(
+        curvature=CurvatureQCConfig(threshold=5.0),
+        minimum_radius=MinimumRadiusQCConfig(enabled=False),
+    )
+
+    edges.run_qc(config)
+
     assert QCFlag.MINIMUM_RADIUS not in detection.qc.flags
+
+
+@pytest.mark.parametrize(
+    "invalid_radius",
+    [0.0, -1.0, np.nan, np.inf, -np.inf],
+)
+def test_minimum_radius_qc_rejects_invalid_radii(invalid_radius):
+    detection = edge([5.0] * 8)
+    object.__setattr__(
+        detection.full_contour,
+        "r",
+        np.full(8, invalid_radius),
+    )
+
+    check_minimum_radius(detection, 5.0)
+
+    assert QCFlag.MINIMUM_RADIUS in detection.qc.flags
